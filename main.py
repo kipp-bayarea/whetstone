a06adde7ee86102a6ae0fa734745d47b6f02948c
dhess <dan.hess@kippbayarea.org>
2020-06-18 10:46:54 -0700

    Add write to sqlite db (for testing)

--- main.py
+++ main.py
@@ -1,49 +1,10 @@
-from whetstone import Whetstone
-import json
-from os import path, makedirs
-
-
-def extract(api, endpoint, tag=False):
-    filename = f"data/{endpoint}.json"
-    if path.exists(filename):
-        pass
-    else:
-        data = api.get_all(endpoint, tag=tag)
-        if not path.exists("data"):
-            makedirs("data")
-        with open(filename, "w") as f:
-            f.write(json.dumps(data, indent=2))
+from whetstone import Users
+from sqlsorcery import SQLite
 
 
 def main():
-    api = Whetstone()
-    endpoints = [
-        "assignments",
-        "informals",
-        "measurements",
-        "meetings",
-        "observations",
-        "rubrics",
-        "schools",
-        "users",
-    ]
-
-    for endpoint in endpoints:
-        extract(api, endpoint)
-
-    tags = [
-        "grades",
-        "courses",
-        "measurementgroups",
-        "goaltypes",
-        "meetingtypes",
-        "observationtypes",
-        "assignmenttypes",
-        "assignmentpresets",
-    ]
-
-    for tag in tags:
-        extract(api, tag, tag=True)
+    sql = SQLite(path="test.db")
+    Users(sql).import_all()
 
 
 if __name__ == "__main__":
