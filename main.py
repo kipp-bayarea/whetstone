505dc5cefb730d9df0d6fec0c12ca3f3601d0a67
dhess <dan.hess@kippbayarea.org>
2020-06-17 17:00:43 -0700

    Add all needed endpoints and tags

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 from whetstone import Whetstone
 import json
+from os import path, makedirs
+
+
+def extract(api, endpoint, tag=False):
+    filename = f"data/{endpoint}.json"
+    if path.exists(filename):
+        pass
+    else:
+        data = api.get_all(endpoint, tag=tag)
+        if not path.exists("data"):
+            makedirs("data")
+        with open(filename, "w") as f:
+            f.write(json.dumps(data, indent=2))
 
 
 def main():
     api = Whetstone()
-    users = api.get_all("users")
-    with open("users.json", "w") as f:
-        f.write(json.dumps(users))
+    endpoints = [
+        "assignments",
+        "informals",
+        "measurements",
+        "meetings",
+        "observations",
+        "rubrics",
+        "schools",
+        "users",
+    ]
+
+    for endpoint in endpoints:
+        extract(api, endpoint)
+
+    tags = [
+        "grades",
+        "courses",
+        "measurementgroups",
+        "goaltypes",
+        "meetingtypes",
+        "observationtypes",
+        "assignmenttypes",
+        "assignmentpresets",
+    ]
+
+    for tag in tags:
+        extract(api, tag, tag=True)
 
 
 if __name__ == "__main__":
