681f165cd29d02603044dcdbde99c0f78500364f
dhess <dan.hess@kippbayarea.org>
2020-06-18 19:23:55 -0700

    Refactor preprocessing to split request into many models

--- whetstone.py
+++ whetstone.py
@@ -13,11 +13,15 @@ class CredentialError(Exception):
 
 class Whetstone:
     def __init__(self, sql, qa=False):
-        self.endpoint = self.__class__.__name__
-        self.table_name = f"whetstone_{self.endpoint}"
-        self.filename = f"data/{self.endpoint}.json"
+        subdomain = "api-qa" if qa else "api"
+        self.url = f"https://{subdomain}.whetstoneeducation.com"
         self.client_id = os.getenv("CLIENT_ID")
         self.client_secret = os.getenv("CLIENT_SECRET")
+        self.token = self._authorize()
+        self.endpoint = self.__class__.__name__
+        self.filename = f"data/{self.endpoint}.json"
+        self.sql = sql
+        self.tag = False
         self.columns = []
         self.dates = [
             "archivedAt",
@@ -29,11 +33,6 @@ class Whetstone:
             "lastPublished",
             "observedAt",
         ]
-        self.tag = False
-        self.sql = sql
-        subdomain = "api-qa" if qa else "api"
-        self.url = f"https://{subdomain}.whetstoneeducation.com"
-        self.token = self._authorize()
 
     def _authorize(self):
         auth_url = f"{self.url}/auth/client/token"
@@ -67,8 +66,9 @@ class Whetstone:
         else:
             raise Exception(f"Failed to list {self.endpoint}")
 
-    def _write_to_db(self, df):
-        self.sql.insert_into(self.table_name, df, chunksize=10000, if_exists="replace")
+    def _write_to_db(self, df, model):
+        tablename = f"whetstone_{model}"
+        self.sql.insert_into(tablename, df, chunksize=10000, if_exists="replace")
 
     def _write_to_json(self, data):
         if os.path.exists(self.filename):
@@ -76,40 +76,24 @@ class Whetstone:
         with open(self.filename, "w") as f:
             json.dump(data, f, indent=2)
 
-    def import_all(self):
+    def transform_and_load(self):
         data = self.get_all()
-        self._write_to_json(data)
-        self.additional_imports(data)
-        df = self._process_and_filter_records(data)
-        self._write_to_db(df)
+        models = self._preprocess_records(data)
+        for model, records in models.items():
+            df = pd.DataFrame(records)
+            df = df.astype("object")
+            df = self._convert_dates(df)
+            df.rename(columns={"_id": "id"}, inplace=True)
+            self._write_to_db(df, model)
 
     def _preprocess_records(self, records):
         return records
-
-    def _process_and_filter_records(self, records):
-        new_records = self._preprocess_records(records)
-        df = pd.json_normalize(new_records)
-        df = df.reindex(columns=self.columns)
-        df = df.astype("object")
-        df = self._convert_dates(df)
-        df.rename(columns={"_id": "id"}, inplace=True)
-        return df
 
     def _convert_dates(self, df):
         date_types = {col: "datetime64[ns]" for col in df.columns if col in self.dates}
         if date_types:
             df = df.astype(date_types)
         return df
-
-    def additional_imports(self, records):
-        pass
-
-    def extract_subrecords(self, subrecords, tablename):
-        df = pd.DataFrame(subrecords)
-        self._convert_dates(df)
-        self.sql.insert_into(
-            f"whetstone_{tablename}", df, chunksize=10000, if_exists="replace"
-        )
 
 
 class Users(Whetstone):
@@ -344,13 +328,15 @@ class Measurements(Whetstone):
             "rowStyle",
         ]
 
-    def additional_imports(self, records):
-        measurement_options_records = []
+    def _preprocess_records(self, records):
+        models = {"Measurements": [], "MeasurementOptions": []}
         for record in records:
-            measurement = record.get("_id")
+            record_id = record.get("_id")
+            measurement = {k: v for (k, v) in record.items() if k in self.columns}
+            models["Measurements"].append(measurement)
             options = record.get("measurementOptions")
             if options:
-                options = [dict(item, measurment=measurement) for item in options]
-                measurement_options_records.extend(options)
-        self.extract_subrecords(measurement_options_records, "MeasurementOptions")
+                options = [dict(item, measurement=record_id) for item in options]
+                models["MeasurementOptions"].extend(options)
+        return models
 
