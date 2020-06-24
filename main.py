51e78c5b02d94da2668abfb3434ce041703b80d8
dhess <dan.hess@kippbayarea.org>
2020-06-24 12:40:42 -0700

    Simplify tag calls

--- main.py
+++ main.py
@@ -13,28 +13,33 @@ def main():
     whetstone.Informals(sql).transform_and_load()
     whetstone.Rubrics(sql).transform_and_load()
 
-    whetstone.Tag(sql, "courses").transform_and_load()
-    whetstone.Tag(sql, "tags").transform_and_load()
-    whetstone.Tag(sql, "grades").transform_and_load()
-    whetstone.Tag(sql, "measurement_groups").transform_and_load()
-    whetstone.Tag(sql, "goal_types").transform_and_load()
-    whetstone.Tag(sql, "meeting_types").transform_and_load()
-    whetstone.Tag(sql, "observation_types").transform_and_load()
-    whetstone.Tag(sql, "assignment_types").transform_and_load()
-    whetstone.Tag(sql, "assignment_presets").transform_and_load()
-    whetstone.Tag(sql, "user_types").transform_and_load()
-    whetstone.Tag(sql, "coaching_move_types").transform_and_load()
-    whetstone.Tag(sql, "collaboration_types").transform_and_load()
-    whetstone.Tag(sql, "measurement_types").transform_and_load()
-    whetstone.Tag(sql, "meeting_modules").transform_and_load()
-    whetstone.Tag(sql, "meeting_standards").transform_and_load()
-    whetstone.Tag(sql, "observation_labels").transform_and_load()
-    whetstone.Tag(sql, "observation_modules").transform_and_load()
-    whetstone.Tag(sql, "observation_types").transform_and_load()
-    whetstone.Tag(sql, "periods").transform_and_load()
-    whetstone.Tag(sql, "tracks").transform_and_load()
-    whetstone.Tag(sql, "plu_content_areas").transform_and_load()
-    whetstone.Tag(sql, "plu_event_types").transform_and_load()
+    tags = [
+        "courses",
+        "tags",
+        "grades",
+        "measurement_groups",
+        "goal_types",
+        "meeting_types",
+        "observation_types",
+        "assignment_types",
+        "assignment_presets",
+        "user_types",
+        "coaching_move_types",
+        "collaboration_types",
+        "measurement_types",
+        "meeting_modules",
+        "meeting_standards",
+        "observation_labels",
+        "observation_modules",
+        "observation_types",
+        "periods",
+        "tracks",
+        "plu_content_areas",
+        "plu_event_types",
+    ]
+
+    for tag in tags:
+        whetstone.Tag(sql, tag).transform_and_load()
 
 
 if __name__ == "__main__":
