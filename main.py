947021fa211b322bc5d86b0ffba57bd44ff8ae7b
D.C. Hess <dan.hess@kippbayarea.org>
2020-06-29 10:17:04 -0700

    Endpoints (#2)
    Depends on #1
    
    Adds logic for writing endpoints to database parsed into a relation model.

--- main.py
+++ main.py
@@ -1,50 +1,73 @@
-from whetstone import Whetstone
-import json
-from os import path, makedirs
+import logging
+import os
+import sys
+import traceback
+import whetstone
+from sqlsorcery import MSSQL
+from mailer import Mailer
 
 
-def extract(api, endpoint, tag=False):
-    filename = f"data/{endpoint}.json"
-    if path.exists(filename):
-        pass
-    else:
-        data = api.get_all(endpoint, tag=tag)
-        if not path.exists("data"):
-            makedirs("data")
-        with open(filename, "w") as f:
-            f.write(json.dumps(data, indent=2))
+def configure_logging():
+    logging.basicConfig(
+        handlers=[
+            logging.FileHandler(filename="data/app.log", mode="w+"),
+            logging.StreamHandler(sys.stdout),
+        ],
+        level=logging.DEBUG,
+        format="%(asctime)s | %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %I:%M:%S%p %Z",
+    )
+    logging.getLogger("requests").setLevel(logging.ERROR)
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
 
 
 def main():
-    api = Whetstone()
-    endpoints = [
-        "assignments",
-        "informals",
-        "measurements",
-        "meetings",
-        "observations",
-        "rubrics",
-        "schools",
-        "users",
-    ]
-
-    for endpoint in endpoints:
-        extract(api, endpoint)
+    configure_logging()
+    sql = MSSQL()
+    whetstone.Users(sql).transform_and_load()
+    whetstone.Schools(sql).transform_and_load()
+    whetstone.Meetings(sql).transform_and_load()
+    whetstone.Observations(sql).transform_and_load()
+    whetstone.Measurements(sql).transform_and_load()
+    whetstone.Assignments(sql).transform_and_load()
+    whetstone.Informals(sql).transform_and_load()
+    whetstone.Rubrics(sql).transform_and_load()
 
     tags = [
-        "grades",
         "courses",
-        "measurementgroups",
-        "goaltypes",
-        "meetingtypes",
-        "observationtypes",
-        "assignmenttypes",
-        "assignmentpresets",
+        "tags",
+        "grades",
+        "measurement_groups",
+        "goal_types",
+        "meeting_types",
+        "observation_types",
+        "assignment_types",
+        "assignment_presets",
+        "user_types",
+        "coaching_move_types",
+        "collaboration_types",
+        "measurement_types",
+        "meeting_modules",
+        "meeting_standards",
+        "observation_labels",
+        "observation_modules",
+        "observation_types",
+        "periods",
+        "tracks",
+        "plu_content_areas",
+        "plu_event_types",
     ]
 
     for tag in tags:
-        extract(api, tag, tag=True)
+        whetstone.Tag(sql, tag).transform_and_load()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+        error_message = None
+    except Exception as e:
+        logging.exception(e)
+        error_message = traceback.format_exc()
+    if int(os.getenv("ENABLE_MAILER", default=0)):
+        Mailer("Whetstone Connector").notify(error_message=error_message)
